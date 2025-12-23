6fbb5074137c18104d27b74e22955a3c8791a01c
sholay1234 <sholay1104@gmail.com>
2025-12-23 07:02:59 +0530

    Update codebase for migration to deedblock_v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     description: "Powered By Project BlockChain",
     images: ["/logo2.png"],
   },
-  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")),
+  metadataBase: new URL("http://localhost:3000"), // Set base URL for metadata resolving
 };
 
 const dmSans = DM_Sans({
